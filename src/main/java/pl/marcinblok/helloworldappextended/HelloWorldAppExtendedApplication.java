8957d5d823cc24022dc36fafb6599a35ebfa47a5
Marcin-Blok <marcinblok7980@gmail.com>
2021-02-17 02:16:37 +0100

    Stworzenie projektu

---
+++ src/main/java/pl/marcinblok/helloworldappextended/HelloWorldAppExtendedApplication.java
@@ -0,0 +1,13 @@
+package pl.marcinblok.helloworldappextended;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAppExtendedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAppExtendedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcinblok/helloworldappextended/HelloWorldAppExtendedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcinblok.helloworldappextended;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppExtendedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
